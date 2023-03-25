064247a0625c1b1e78fa9aff1e744c8eff2b30f9
Kate Rataiko <kate94.08@mail.ru>
2023-03-26 00:01:08 +0300

    руб.мес

--- name/src/components/contactCard.jsx
+++ name/src/components/contactCard.jsx
@@ -5,7 +5,10 @@ function Card(props) {
     return (
         < div className={"contact-card " + props.color}>
             <div className="card-name">{props.name}</div>
-            <div className="card-tarif"> {props.tarif}</div>
+            <div className="card-tarif">
+                <div className='dengi'>руб</div>
+                <div className='mesyaz'>{props.tarif}</div>/мес
+            </div>
             <div className="card-razmer">{props.razmer}</div>
             <div className="card-traffic">{props.traffic}</div>
         </div >
